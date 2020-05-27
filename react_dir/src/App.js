fe4c96d889fd173554b334c1d4b0ae99df44ac42
clemhff <clemthebest57@gmail.com>
2020-05-27 18:47:33 +0200

    first

---
+++ react_dir/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './Menu';
+import Quotes_List from './Quotes_List';
+
+function App() {
+  return (
+    <div class="App">
+      <Menu/>
+      <Quotes_List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_dir/src/Quotes_List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Quotes_List() {
+  return (
+    <div class="title_div">
+      <h1 class="title">premier titre</h1>
+    </div>
+  );
+}
+
+export default Quotes_List;
