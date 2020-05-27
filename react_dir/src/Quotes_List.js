4c8ad6ce5dc46548eaca04b7c7290683a87e9d73
clemhff <clemthebest57@gmail.com>
2020-05-27 19:15:08 +0200

    title react

--- react_dir/src/Quotes_List.js
+++ react_dir/src/Quotes_List.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Quotes_List() {
   return (
     <div class="title_div">
-      <h1 class="title">premier titre</h1>
+      <h1 class="title">premier titre React</h1>
     </div>
   );
 }
