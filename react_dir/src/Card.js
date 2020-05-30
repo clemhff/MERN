166827edc8ff9a1b3166e931793bcdcfef802a7b
clemhff <clemthebest57@gmail.com>
2020-05-31 00:48:31 +0200

    modification in cards state

--- react_dir/src/Card.js
+++ react_dir/src/Card.js
@@ -3,32 +3,115 @@ import './css/Card.css';
 
 class Card extends Component {
 
-  render () {
-    return (
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{this.props.quote_Object.quote}</h5>
-          <p className="card-text">{this.props.quote_Object.author}</p>
-          <button
-            type="button"
-            className="btn btn-primary btn-modify"
-            onClick={() => {
-              // Onclick on "Modify" button
-              this.props.onClickM(this.props.num);
-            }}
-          >Modify</button>
-          <button
-            type="button"
-            className="btn btn-danger btn-delete"
-            onClick={() => {
-              // Onclick on "Modify" button
-              this.props.onClickD(this.props.num);
-            }}
-          >Delete</button>
+
+
+  displayCard = () => {
+
+    if(this.props.quote_Object.cardState === 'ok') {
+      return (
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">{this.props.quote_Object.quote}</h5>
+            <p className="card-text">{this.props.quote_Object.author}</p>
+            <button
+              type="button"
+              className="btn btn-primary btn-modify"
+              onClick={() => {
+                // Onclick on "Modify" button
+                this.props.onClickM(this.props.num);
+              }}
+            >Modify</button>
+            <button
+              type="button"
+              className="btn btn-danger btn-delete"
+              onClick={() => {
+                // Onclick on "Modify" button
+                this.props.onClickD(this.props.num);
+              }}
+            >Delete</button>
+          </div>
         </div>
-      </div>
-    );
-  }
+      );
+    } //close if
+
+    else if (this.props.quote_Object.cardState === 'delete') {
+      return (
+        null
+      );//close else if delete
+    }
+
+    else if (this.props.quote_Object.cardState === 'modify') {
+      return (
+        <div className="card">
+          <div className="card-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor={'quoteForm'+ this.props.num}>Quote</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id={'quoteForm'+ this.props.num}
+                  defaultValue={this.props.quote_Object.quote}
+                  placeholder="Enter quote"
+                  onChange={(event) => {
+                    event.preventDefault();
+                    const q = event.target;
+                    //console.log(JSON.stringify(q.value));
+                    this.props.changeForm(0, q.value);
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor={'quoteForm'+ this.props.num}>Author</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id={'authorForm'+ this.props.num}
+                  defaultValue={this.props.quote_Object.author}
+                  placeholder="Enter author"
+                  onChange={(event) => {
+                    event.preventDefault();
+                    const a = event.target;
+                    //console.log(JSON.stringify(a.value));
+                    this.props.changeForm(1, a.value);
+                  }}
+                />
+              </div>
+              <button
+                className="btn btn-primary btn-modify"
+                onClick={(e) => {
+                  // Onclick submit form
+                  e.preventDefault();
+                  e.stopPropagation();
+                  this.props.onClickS(this.props.num);
+                }}
+              >Submit</button>
+              <button
+                className="btn btn-danger btn-delete"
+                onClick={(e) => {
+                  // Onclick submit form
+                  e.preventDefault();
+                  e.stopPropagation();
+                  this.props.onClickC(this.props.num);
+                }}
+              >Cancel</button>
+            </form>
+          </div>
+        </div>
+      );
+    }////close else if modify
+
+    else {
+      return (
+        alert('error')
+      );
+    }//close else error
+
+  }//close displayCard
+
+  render () {
+    return (this.displayCard());
+  } //close render
 
 
 }
--- react_dir/src/QuotesList.js
+++ react_dir/src/QuotesList.js
@@ -7,11 +7,12 @@ class QuotesList extends Component {
       super(props);
       this.state = {
         cards: [],
-        stepNumber: 0,
-        xIsNext: true
+        formInput: [],
+        formState: false
       };
   }
 
+  //API call
   componentDidMount() {
       fetch('http://localhost:8080/lastentries')
         .then(response => response.json())
@@ -25,10 +26,10 @@ class QuotesList extends Component {
 
         })
         .then(data => this.setState({ cards : data }))
-
-        ;
+      ;
    }
 
+   //Créer les cards
    createCards() {
      this.state.cards.map((index) => {
 
@@ -37,15 +38,23 @@ class QuotesList extends Component {
      })
    }
 
+   //Supprimer l'affichage de la card
    modifyCard = (i) => {
      const temp = this.state.cards.slice();
+     temp.map( (x) => {
+       if (x.cardState === 'modify'){
+         x.cardState ='ok'
+       }
+       return x ;
+     });
      temp[i].cardState ='modify';
      console.log(JSON.stringify(temp[i].cardState));
-     this.setState({ cards: temp });
+     this.setState({ cards: temp, formInput: [], formState: false });
 
      return ;
    }
 
+   //Supprimer l'affichage de la card
    deleteCard = (i) => {
      const temp = this.state.cards.slice();
      temp[i].cardState ='delete';
@@ -53,6 +62,53 @@ class QuotesList extends Component {
      this.setState({ cards: temp });
 
      return ;
+   }
+
+   //fonstion qui prend les valeurs de formInput et qui l'insert dans cards props
+   submitForm = (i) => {
+     console.log('submitting ' + i);
+     if(this.state.formState){
+       const temp = this.state.cards.slice();
+       temp[i].cardState ='ok';
+       if (this.state.formInput[1] != null){
+         temp[i].author = this.state.formInput[1];
+       }
+       if (this.state.formInput[0] != null){
+         temp[i].quote = this.state.formInput[0];
+       }
+       this.setState({ cards: temp, formInput: [], formState: false });
+
+     }
+     else {
+       const temp = this.state.cards.slice();
+       temp[i].cardState ='ok';
+       console.log(JSON.stringify(temp[i].cardState));
+       this.setState({ cards: temp, formInput: [], formState: false });
+     }
+   }
+
+   cancelForm = (i) => {
+     const temp = this.state.cards.slice();
+     temp[i].cardState ='ok';
+     this.setState({ cards: temp, formInput: [], formState: false });
+   }
+
+   //fonction qui alimente formInput props pour les input du formaulaire
+   dataForm = (cas, value) => {
+     this.setState({ formState: true });
+     if (cas === 0){
+       const q = this.state.formInput.slice();
+       q[0]=value;;
+       this.setState({ formInput: q });
+     }
+     else if (cas === 1){
+       const q = this.state.formInput.slice();
+       q[1]=value;;
+       this.setState({ formInput: q });
+     }
+     else {
+       console.log('pas d\'input error');
+     }
    }
 
   render () {
@@ -66,6 +122,9 @@ class QuotesList extends Component {
             quote_Object={this.state.cards[i]}
             onClickM={(i) => this.modifyCard(i)}
             onClickD={(i) => this.deleteCard(i)}
+            onClickS={(i) => this.submitForm(i)}
+            onClickC={(i) => this.cancelForm(i)}
+            changeForm={(cas, value) => this.dataForm(cas, value)}
           />
       ))
 
